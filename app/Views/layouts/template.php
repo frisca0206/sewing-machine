370abf3a42497c7d39e2f818488726af6e38cff7
PKL (GLA) <friscafebrianti16@gmail.com>
2023-09-20 14:58:09 +0700

    add masterdata

--- app/Views/layouts/template.php
+++ app/Views/layouts/template.php
@@ -13,36 +13,36 @@
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="<?= base_url('adminLTE'); ?>/plugins/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="<?= base_url('assets'); ?>/plugins/fontawesome-free/css/all.min.css">
     <!-- Ionicons -->
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
     <!-- Select2 -->
-    <link rel="stylesheet" href="<?= base_url('adminLTE'); ?>/plugins/select2/css/select2.min.css">
-    <link rel="stylesheet" href="<?= base_url('adminLTE'); ?>/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
+    <link rel="stylesheet" href="<?= base_url('assets'); ?>/plugins/select2/css/select2.min.css">
+    <link rel="stylesheet" href="<?= base_url('assets'); ?>/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
     <!-- Tempusdominus Bootstrap 4 -->
     <link rel="stylesheet"
-        href="<?= base_url('adminLTE'); ?>/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+        href="<?= base_url('assets'); ?>/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
     <!-- iCheck -->
-    <link rel="stylesheet" href="<?= base_url('adminLTE'); ?>/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+    <link rel="stylesheet" href="<?= base_url('assets'); ?>/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
     <!-- JQVMap -->
-    <link rel="stylesheet" href="<?= base_url('adminLTE'); ?>/plugins/jqvmap/jqvmap.min.css">
+    <link rel="stylesheet" href="<?= base_url('assets'); ?>/plugins/jqvmap/jqvmap.min.css">
     <!-- Theme style -->
-    <link rel="stylesheet" href="<?= base_url('adminLTE'); ?>/css/adminlte.min.css">
+    <link rel="stylesheet" href="<?= base_url('assets'); ?>/css/adminlte.min.css">
 
     <!-- SweetAlert2 -->
-    <link rel="stylesheet" href="<?= base_url('adminLTE'); ?>/plugins/sweetalert2/sweetalert2.min.css">
+    <link rel="stylesheet" href="<?= base_url('assets'); ?>/plugins/sweetalert2/sweetalert2.min.css">
 
     <!-- overlayScrollbars -->
-    <link rel="stylesheet" href="<?= base_url('adminLTE'); ?>/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+    <link rel="stylesheet" href="<?= base_url('assets'); ?>/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
     <!-- Daterange picker -->
-    <link rel="stylesheet" href="<?= base_url('adminLTE'); ?>/plugins/daterangepicker/daterangepicker.css">
+    <link rel="stylesheet" href="<?= base_url('assets'); ?>/plugins/daterangepicker/daterangepicker.css">
     <!-- summernote -->
-    <link rel="stylesheet" href="<?= base_url('adminLTE'); ?>/plugins/summernote/summernote-bs4.min.css">
+    <link rel="stylesheet" href="<?= base_url('assets'); ?>/plugins/summernote/summernote-bs4.min.css">
     
     <!-- DataTables -->
-    <link rel="stylesheet" href="<?= base_url('adminLTE'); ?>/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
-    <link rel="stylesheet" href="<?= base_url('adminLTE'); ?>/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
-    <link rel="stylesheet" href="<?= base_url('adminLTE'); ?>/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+    <link rel="stylesheet" href="<?= base_url('assets'); ?>/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="<?= base_url('assets'); ?>/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+    <link rel="stylesheet" href="<?= base_url('assets'); ?>/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
     
     <!-- Custom App CSS -->
     <link rel="stylesheet" href="<?= base_url(''); ?>css/app.css">
@@ -60,7 +60,7 @@
 
         <!-- Preloader -->
         <div class="preloader flex-column justify-content-center align-items-center">
-            <img class="animation__shake" src="<?= base_url('adminLTE'); ?>/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+            <img class="animation__shake" src="<?= base_url('assets'); ?>/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
         </div>
 
         <!-- ## Section -->
@@ -75,60 +75,60 @@
     <!-- ./wrapper -->
 
     <!-- jQuery -->
-    <script src="<?= base_url('adminLTE'); ?>/plugins/jquery/jquery.min.js"></script>
+    <script src="<?= base_url('assets'); ?>/plugins/jquery/jquery.min.js"></script>
     <!-- jQuery UI 1.11.4 -->
-    <script src="<?= base_url('adminLTE'); ?>/plugins/jquery-ui/jquery-ui.min.js"></script>
+    <script src="<?= base_url('assets'); ?>/plugins/jquery-ui/jquery-ui.min.js"></script>
     <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
     <script>
     $.widget.bridge('uibutton', $.ui.button)
     </script>
     <!-- Bootstrap 4 -->
-    <script src="<?= base_url('adminLTE'); ?>/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="<?= base_url('assets'); ?>/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 
     <!-- Select2 -->
-    <script src="<?= base_url('adminLTE'); ?>/plugins/select2/js/select2.full.min.js"></script>
+    <script src="<?= base_url('assets'); ?>/plugins/select2/js/select2.full.min.js"></script>
 
     <!-- ChartJS -->
-    <script src="<?= base_url('adminLTE'); ?>/plugins/chart.js/Chart.min.js"></script>
+    <script src="<?= base_url('assets'); ?>/plugins/chart.js/Chart.min.js"></script>
     <!-- Sparkline -->
-    <script src="<?= base_url('adminLTE'); ?>/plugins/sparklines/sparkline.js"></script>
+    <script src="<?= base_url('assets'); ?>/plugins/sparklines/sparkline.js"></script>
 
     <!-- JQVMap -->
-    <script src="<?= base_url('adminLTE'); ?>/plugins/jqvmap/jquery.vmap.min.js"></script>
-    <script src="<?= base_url('adminLTE'); ?>/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+    <script src="<?= base_url('assets'); ?>/plugins/jqvmap/jquery.vmap.min.js"></script>
+    <script src="<?= base_url('assets'); ?>/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
     
     <!-- jQuery Knob Chart -->
-    <script src="<?= base_url('adminLTE'); ?>/plugins/jquery-knob/jquery.knob.min.js"></script>
+    <script src="<?= base_url('assets'); ?>/plugins/jquery-knob/jquery.knob.min.js"></script>
     <!-- daterangepicker -->
-    <script src="<?= base_url('adminLTE'); ?>/plugins/moment/moment.min.js"></script>
-    <script src="<?= base_url('adminLTE'); ?>/plugins/daterangepicker/daterangepicker.js"></script>
+    <script src="<?= base_url('assets'); ?>/plugins/moment/moment.min.js"></script>
+    <script src="<?= base_url('assets'); ?>/plugins/daterangepicker/daterangepicker.js"></script>
     <!-- Tempusdominus Bootstrap 4 -->
-    <script src="<?= base_url('adminLTE'); ?>/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+    <script src="<?= base_url('assets'); ?>/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
     <!-- Summernote -->
-    <script src="<?= base_url('adminLTE'); ?>/plugins/summernote/summernote-bs4.min.js"></script>
+    <script src="<?= base_url('assets'); ?>/plugins/summernote/summernote-bs4.min.js"></script>
     <!-- overlayScrollbars -->
-    <script src="<?= base_url('adminLTE'); ?>/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+    <script src="<?= base_url('assets'); ?>/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
     
     <!-- SweetAlert2 -->
-    <script src="<?= base_url('adminLTE'); ?>/plugins/sweetalert2/sweetalert2.min.js"></script>
+    <script src="<?= base_url('assets'); ?>/plugins/sweetalert2/sweetalert2.min.js"></script>
 
     <!-- DataTables  & Plugins -->
-    <script src="<?= base_url('adminLTE'); ?>/plugins/datatables/jquery.dataTables.min.js"></script>
-    <script src="<?= base_url('adminLTE'); ?>/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
-    <script src="<?= base_url('adminLTE'); ?>/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
-    <script src="<?= base_url('adminLTE'); ?>/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
-    <script src="<?= base_url('adminLTE'); ?>/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
-    <script src="<?= base_url('adminLTE'); ?>/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
-    <script src="<?= base_url('adminLTE'); ?>/plugins/jszip/jszip.min.js"></script>
-    <script src="<?= base_url('adminLTE'); ?>/plugins/pdfmake/pdfmake.min.js"></script>
-    <script src="<?= base_url('adminLTE'); ?>/plugins/pdfmake/vfs_fonts.js"></script>
-    <script src="<?= base_url('adminLTE'); ?>/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
-    <script src="<?= base_url('adminLTE'); ?>/plugins/datatables-buttons/js/buttons.print.min.js"></script>
-    <script src="<?= base_url('adminLTE'); ?>/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+    <script src="<?= base_url('assets'); ?>/plugins/datatables/jquery.dataTables.min.js"></script>
+    <script src="<?= base_url('assets'); ?>/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+    <script src="<?= base_url('assets'); ?>/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+    <script src="<?= base_url('assets'); ?>/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+    <script src="<?= base_url('assets'); ?>/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+    <script src="<?= base_url('assets'); ?>/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+    <script src="<?= base_url('assets'); ?>/plugins/jszip/jszip.min.js"></script>
+    <script src="<?= base_url('assets'); ?>/plugins/pdfmake/pdfmake.min.js"></script>
+    <script src="<?= base_url('assets'); ?>/plugins/pdfmake/vfs_fonts.js"></script>
+    <script src="<?= base_url('assets'); ?>/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+    <script src="<?= base_url('assets'); ?>/plugins/datatables-buttons/js/buttons.print.min.js"></script>
+    <script src="<?= base_url('assets'); ?>/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
 
     
     <!-- AdminLTE App -->
-    <script src="<?= base_url('adminLTE'); ?>/js/adminlte.js"></script>
+    <script src="<?= base_url('assets'); ?>/js/adminlte.js"></script>
     
     
     <!-- Custom App JS -->
@@ -140,6 +140,9 @@
 
     <script>
         $(document).ready(function() {
+             
+            $('.masterdata-datatable').DataTable({});
+
             /*** add active class and stay opened when selected ***/
             var url = window.location;
     
